b988f5cc83025d0943aceef24a7c268d0112d2d5
RahulGupta <rahulrgd21@gmail.com>
2024-07-04 09:41:54 +0530

    Completed the Producer app

--- delivery-boy/src/main/java/com/deliveryboy/DeliveryBoyApplication.java
+++ delivery-boy/src/main/java/com/deliveryboy/DeliveryBoyApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DeliveryBoyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeliveryBoyApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(DeliveryBoyApplication.class, args);
+  }
 }
